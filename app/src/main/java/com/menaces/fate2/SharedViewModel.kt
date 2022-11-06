75f2769b58bada62f5ad46e45f0dd3f5c52a8284
twila ternida <twilaternida2123@gmail.com>
2022-11-06 15:05:13 -0600

    Now updates narration using a list from the UnexpectedEncounterStory.kt file (in data), but counter doesn't progress past 1 yet as counting is not handled. Images also not handled yet

--- app/src/main/java/com/menaces/fate2/SharedViewModel.kt
+++ app/src/main/java/com/menaces/fate2/SharedViewModel.kt
@@ -1,18 +1,30 @@
 package com.menaces.fate2
 
-import android.widget.Toast
+import android.graphics.drawable.Drawable
+import android.media.Image
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.menaces.fate2.data.UnexpectedEncounterStory
 
 class SharedViewModel : ViewModel() {
 
-    private val storyText: List<String> = listOf("Narration 1", "Narration 2", "Narration 3")
+//    private val storyText: List<String> = listOf("Narration 1", "Narration 2", "Narration 3")
+    private val unexpectedEncounterText: List<String> = UnexpectedEncounterStory.narration
+
+//    private val storyImages: List<Int> = listOf(R.drawable.trees_story, R.drawable.unexpected_encounter_image)
 
     // variable to contain message whenever it gets changed/modified(mutable)
     val message = MutableLiveData<String>()
+//    val image = MutableLiveData<Image>()
 
-    // function to send message
+    // function to updated narration
     fun updateNarration(textID: Int) {
-        message.value = storyText[textID]
+        message.value = unexpectedEncounterText[textID]
+    }
+
+    // function to update image
+    fun updateImage(imageID: Int) {
+//        image.value =
+//        storyImages[imageID]
     }
 }
--- app/src/main/java/com/menaces/fate2/data/UnexpectedEncounterStory.kt
+++ app/src/main/java/com/menaces/fate2/data/UnexpectedEncounterStory.kt
@@ -4,7 +4,7 @@ import android.media.Image
 
 object UnexpectedEncounterStory {
     // Narration strings for each screen of the story
-    val narration: List<String> = listOf()
+    val narration: List<String> = listOf("Narration 1", "Narration 2", "Narration 3")
 
     // Images for each screen of the story
     val images: List<Image> = listOf()
