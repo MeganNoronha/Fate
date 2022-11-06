f82d5844c6f5d2c4d81522badbd0815c2eb08cb1
twila ternida <twilaternida2123@gmail.com>
2022-11-06 16:26:53 -0600

    Removed comments

--- app/src/main/java/com/menaces/fate2/data/UnexpectedEncounterStory.kt
+++ app/src/main/java/com/menaces/fate2/data/UnexpectedEncounterStory.kt
@@ -5,12 +5,6 @@ import com.menaces.fate2.R
 import com.menaces.fate2.model.Screen
 
 object UnexpectedEncounterStory {
-    // Narration strings for each screen of the story
-//    val narration: List<String> = listOf("Narration 1", "Narration 2", "Narration 3")
-
-    // Images for each screen of the story
-//    val images: List<Int> = listOf(R.drawable.trees_story, R.drawable.unexpected_encounter_image, R.drawable.bittern)
-
     val screens: List<Screen> = listOf(
         Screen(
             "Narration 1",
