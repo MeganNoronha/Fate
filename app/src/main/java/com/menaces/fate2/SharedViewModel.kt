334487c6922f813134deb1a31d23f0efe0b9a570
twila ternida <twilaternida2123@gmail.com>
2022-11-06 15:17:17 -0600

    Now updates image based on the list in UnexpectedEncounterStory.kt

--- app/src/main/java/com/menaces/fate2/SharedViewModel.kt
+++ app/src/main/java/com/menaces/fate2/SharedViewModel.kt
@@ -1,6 +1,7 @@
 package com.menaces.fate2
 
 import android.graphics.drawable.Drawable
+import android.media.Image
 import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,11 +9,8 @@ import com.menaces.fate2.data.UnexpectedEncounterStory
 
 class SharedViewModel : ViewModel() {
 
-//    private val storyText: List<String> = listOf("Narration 1", "Narration 2", "Narration 3")
-//    private val storyImages: List<Int> = listOf(R.drawable.trees_story, R.drawable.unexpected_encounter_image)
-
     private val unexpectedEncounterText: List<String> = UnexpectedEncounterStory.narration
-
+    private val unexpectedEncounterImgs: List<Int> = UnexpectedEncounterStory.images
 
     // variable to contain message whenever it gets changed/modified(mutable)
     val message = MutableLiveData<String>()
@@ -21,6 +19,6 @@ class SharedViewModel : ViewModel() {
     // function to updated narration and image
     fun updateScreen(ID: Int) {
         message.value = unexpectedEncounterText[ID]
-        image.value = R.drawable.trees_story
+        image.value = unexpectedEncounterImgs[ID]
     }
 }
--- app/src/main/java/com/menaces/fate2/data/UnexpectedEncounterStory.kt
+++ app/src/main/java/com/menaces/fate2/data/UnexpectedEncounterStory.kt
@@ -1,11 +1,12 @@
 package com.menaces.fate2.data
 
 import android.media.Image
+import com.menaces.fate2.R
 
 object UnexpectedEncounterStory {
     // Narration strings for each screen of the story
     val narration: List<String> = listOf("Narration 1", "Narration 2", "Narration 3")
 
     // Images for each screen of the story
-    val images: List<Image> = listOf()
+    val images: List<Int> = listOf(R.drawable.trees_story, R.drawable.unexpected_encounter_image, R.drawable.bittern)
 }
