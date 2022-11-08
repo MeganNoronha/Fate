6fc18232fd49c6c9b057f5b3ae3abda1a22d47a5
twila ternida <twilaternida2123@gmail.com>
2022-11-08 17:23:51 -0600

    Added first 15 screens of unexpected encounter, navigation not fully working yet

--- app/src/main/java/com/menaces/fate2/data/UnexpectedEncounterStory.kt
+++ app/src/main/java/com/menaces/fate2/data/UnexpectedEncounterStory.kt
@@ -8,61 +8,298 @@ object UnexpectedEncounterStory {
 
     val screens: List<Screen> = listOf(
         Screen(
-            "Narration 1",
+            "You are sitting in the coffee shop, and you really need to get to business " +
+                    "and study for your basket weaving class. Chad is the barista.",
             R.drawable.trees_story,
-            "Left1",
-            "Right1",
+            "",
+            "",
+            1,
             0,
-            0,
-            true
+            false
         ),
         Screen(
-            "Narration 2",
+            "You go up to get a Chai latte with 2 pumps of raspberry, oat milk, 67% ice. " +
+                    "Chad gives you a look and hands you the drink, spelling your name wrong (he " +
+                    "spells “basic”). You can either:",
             R.drawable.unexpected_encounter_image,
-            "",
-            "",
-            0,
-            0,
-            false,
+            "Ask him on a date",
+            "Throw coffee at him",
+            1,
+            2,
+            true,
         ),
         Screen(
-            "Narration 3",
+            "You ask him on a date. He instantly says yes!",
             R.drawable.bittern,
-            "Left2",
-            "Right2",
+            "",
+            "",
+            2,
+            0,
+            false
+        ),
+        Screen(
+            "Surprisingly, he’s into getting hot drinks thrown at him, coffee being his " +
+                    "number one. He asks you on a date.",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
+            1,
+            0,
+            false
+        ),
+        Screen(
+            "You can either:",
+            R.drawable.bittern,
+            "Go rollerblading",
+            "Go to slam poetry",
+            11,
+            1,
+            true
+        ),
+        Screen(
+            "You get ready for slam poetry and meet Chad in front of the " +
+                    "dark alleyway: the entrance to slam poetry. A chill goes up " +
+                    "your spine. Chad, noticing, quickly strips off his jacket and " +
+                    "gently places it around your shoulders. Nudging him with your " +
+                    "elbow, you both enter the land of slam poetry.",
+            R.drawable.bittern,
+            "",
+            "",
+            1,
+            0,
+            false
+        ),
+        Screen(
+            "A man – tall, lean, hair perfectly gelled back – stands in front of the mic. " +
+                    "I look over to Chad and he’s frozen. Who is this man?",
+            R.drawable.bittern,
+            "",
+            "",
+            1,
+            0,
+            false
+        ),
+        Screen(
+            "Mystery Man takes a deep breath and begins to recite his poem:\n" +
+                    "\n" +
+                    "Pick up the phone\n" +
+                    "I’m all alone\n" +
+                    "Chad, what a lad\n" +
+                    "You make me so sad\n" +
+                    "I’m going mad\n" +
+                    "Because of what we had\n" +
+                    "Oh Chad\n" +
+                    "Wouldn’t it be quite rad\n" +
+                    "To go back to the past\n" +
+                    "We had a blast",
+            R.drawable.bittern,
+            "",
+            "",
+            1,
+            0,
+            false
+        ),
+        Screen(
+            "\"Before people have the chance to boo, much less snap their fingers, Chad " +
+                    "leaves my side and quickly rushes to the stage. \n" +
+                    "As if they were actors in the “You Belong with Me” music video, Chad pulls " +
+                    "out a sheet of paper and recites his poem, staring into Mystery Man’s eyes " +
+                    "like they were the last things he’ll ever see:\n" +
+                    "\n" +
+                    "Steve, I didn’t leave\n" +
+                    "How I grieve\n" +
+                    "I can’t believe\n" +
+                    "I didn’t see\n" +
+                    "What was right in front of me\n" +
+                    "Will you marry me?\"",
+            R.drawable.bittern,
+            "",
+            "",
+            1,
+            0,
+            false
+        ),
+        Screen(
+            "\"And with that, Chad is down on one knee, ring in hand. Steve, already in " +
+                    "tears, nods his head yes. They happily embrace. The crowd goes wild. \n" +
+                    "\n" +
+                    "You can either:",
+            R.drawable.bittern,
+            "Stay to congratulate",
+            "Become a nun",
+            1,
+            5,
+            true
+        ),
+        Screen(
+            "Brad comes over and introduces his new fiance. The skinny jeans and sense " +
+                    "of style, you should have known from the start.",
+            R.drawable.bittern,
+            "",
+            "",
+            1,
+            0,
+            false
+        ),
+        Screen(
+            "\"Although you feel slightly betrayed, you give them a smile.\n" +
+                    "“Congratulations to the both of you,” you say, willingly.\"",
+            R.drawable.bittern,
+            "",
+            "",
+            1,
+            0,
+            false
+        ),
+        Screen(
+            "They smile back. Chad the barista and Steve the awful poet, make their way " +
+                    "out of the crowd and into the night, with a future they will share for the " +
+                    "rest of their lives.",
+            R.drawable.bittern,
+            "",
+            "",
+            1,
+            0,
+            false
+        ),
+        Screen(
+            "You let out a little chuckle. This will be a story you tell during drunk " +
+                    "nights and late time highs. Did I tell you about the time I had an " +
+                    "unexpected encounter? ",
+            R.drawable.bittern,
+            "",
+            "",
+            0,
+            0,
+            false
+        ),
+        Screen(
+            "As the crowd cheers, you make your way to the exit. Heartbroken and alone, " +
+                    "you decide to become a nun.\n",
+            R.drawable.bittern,
+            "",
+            "",
+            0,
+            0,
+            false
+        ),
+        Screen(
+            "You dash home, excited to get ready for this date. You Google Meet your " +
+                    "bestie, Priscilla, ready to share the tea about this hot new man.",
+            R.drawable.bittern,
+            "",
+            "",
+            0,
+            0,
+            false
+        ),
+        Screen(
+            "",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
             0,
             0,
             true
-        )
-    )
+        ),
+        Screen(
+            "",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
+            0,
+            0,
+            true
+        ),
+        Screen(
+            "",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
+            0,
+            0,
+            true
+        ),
+        Screen(
+            "",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
+            0,
+            0,
+            true
+        ),
+        Screen(
+            "",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
+            0,
+            0,
+            true
+        ),
+        Screen(
+            "",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
+            0,
+            0,
+            true
+        ),
+        Screen(
+            "",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
+            0,
+            0,
+            true
+        ),Screen(
+            "",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
+            0,
+            0,
+            true
+        ),Screen(
+            "",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
+            0,
+            0,
+            true
+        ),
+        Screen(
+            "",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
+            0,
+            0,
+            true
+        ),
+        Screen(
+            "",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
+            0,
+            0,
+            true
+        ),Screen(
+            "",
+            R.drawable.bittern,
+            "Left3",
+            "Right3",
+            0,
+            0,
+            true
+        ),
 
-//    val screens: List<Screen> = listOf(
-//        Screen(
-//            "Narration 1",
-//            R.drawable.trees_story,
-//            "Left1",
-//            "Right1",
-//            0,
-//            0,
-//            true
-//        ),
-//        Screen(
-//            "Narration 2",
-//            R.drawable.unexpected_encounter_image,
-//            "",
-//            "",
-//            0,
-//            0,
-//            false,
-//        ),
-//        Screen(
-//            "Narration 3",
-//            R.drawable.bittern,
-//            "Left2",
-//            "Right2",
-//            0,
-//            0,
-//            true
-//        )
-//    )
+
+
+    )
 }
