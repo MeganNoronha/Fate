21fd2e29141d915fd6468bfcc036e9bc1f9593b5
KaayaRane <63211165+KaayaRane@users.noreply.github.com>
2022-11-12 22:44:07 -0600

    added media player

--- app/src/main/java/com/menaces/fate2/StartActivity.kt
+++ app/src/main/java/com/menaces/fate2/StartActivity.kt
@@ -1,12 +1,23 @@
 package com.menaces.fate2
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import java.io.IOException
 
 class StartActivity : AppCompatActivity() {
     private lateinit var storyIntent: Intent
+    private lateinit var btnPlay: Button
+    private lateinit var btnPause: Button
+    //var mediaPlayer: MediaPlayer? = null
+    var isPlaying: Boolean = false
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
@@ -23,6 +34,17 @@ class StartActivity : AppCompatActivity() {
             launchInstructions()
         }
 
+        val mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.house_lo)
+
+        // play_music button
+        btnPlay = findViewById(R.id.play_music_button)
+        btnPlay.setOnClickListener {
+            if (!mediaPlayer.isPlaying) {
+                mediaPlayer.start()
+            } else {
+                mediaPlayer.pause()
+            }
+        }
     }
 
     private fun launchStoryMenu() {
