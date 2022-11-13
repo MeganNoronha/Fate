c8d804dfce62c4e4c46a17e3fe6cfb9f208bd56e
katrina28tobias <katrina28tobias@utexas.edu>
2022-11-13 15:27:42 -0600

    set up coming soon screen for tree story

--- app/src/main/java/com/menaces/fate2/data/TreesStory.kt
+++ app/src/main/java/com/menaces/fate2/data/TreesStory.kt
@@ -1,6 +1,8 @@
 package com.menaces.fate2.data
 
 import android.media.Image
+import com.menaces.fate2.R
+import com.menaces.fate2.model.Screen
 
 object TreesStory {
     // Narration strings for each screen of the story
@@ -8,5 +10,17 @@ object TreesStory {
 
     // Images for each screen of the story
     val images: List<Image> = listOf()
+
+    val screens: List<Screen> = listOf(
+        Screen(
+            "COMING SOON",
+            R.drawable.trees_story,
+            "RETURN TO HOME",
+            "",
+            1,
+            0,
+            false
+        ),
+    )
 
 }
