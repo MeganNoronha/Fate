e5a8ae183e03343cef7cbb2dad732de6e9090aef
KaayaRane <63211165+KaayaRane@users.noreply.github.com>
2022-11-17 17:55:30 -0600

    added dynamic color

--- app/src/main/java/com/menaces/fate2/BaseApplication.kt
+++ app/src/main/java/com/menaces/fate2/BaseApplication.kt
@@ -1,6 +1,7 @@
 package com.menaces.fate2
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import com.menaces.fate2.data.StoryDatabase
 import com.menaces.fate2.data.StoryRepository
 import kotlinx.coroutines.CoroutineScope
@@ -11,4 +12,11 @@ class BaseApplication: Application(){
     val applicationScope = CoroutineScope(SupervisorJob())
     val database by lazy {StoryDatabase.getDatabase(this, applicationScope)}
     val repository by lazy {StoryRepository(database.storyDao())}
+
+    override fun onCreate() {
+        super.onCreate()
+        // Apply dynamic color
+        DynamicColors.applyToActivitiesIfAvailable(this, R.style.AppTheme_Overlay)
+        
+    }
 }
