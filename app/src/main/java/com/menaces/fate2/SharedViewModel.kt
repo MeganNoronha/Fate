7ec6200e38db23a1aa73ab7e42897c6aaea6fc86
katrina28tobias <katrina28tobias@utexas.edu>
2022-11-21 23:14:11 -0600

    data saves when app is not completely exited

--- app/src/main/java/com/menaces/fate2/SharedViewModel.kt
+++ app/src/main/java/com/menaces/fate2/SharedViewModel.kt
@@ -9,7 +9,9 @@ import com.menaces.fate2.model.Story
 import androidx.lifecycle.*
 import com.menaces.fate2.data.StoryRepository
 import com.menaces.fate2.model.StoryData
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import com.menaces.fate2.data.StoryDao
 
 class SharedViewModel(private val repository: StoryRepository) : ViewModel() {
 
@@ -37,86 +39,83 @@ class SharedViewModel(private val repository: StoryRepository) : ViewModel() {
     }
 
 
-//    fun updateCounter(counter: Int) {
-//        val story = StoryData(
-//            title = currentStory.title,
-//            counter = counter
-//        )
-//
-//        viewModelScope.launch(Dispatchers.IO) {
-//            // TODO: call the DAO method to update a forageable to the database here
-//            storydao.updateStory(story)
-//        }
-//
-//    }
+    fun updateCounter(storyData: StoryData) {
+        storyData.counter = counter
 
-
-        // variable to contain message whenever it gets changed/modified(mutable)
-        val narration = MutableLiveData<String>()
-        val image = MutableLiveData<Int>()
-        val leftText = MutableLiveData<String>()
-        val rightText = MutableLiveData<String>()
-        val contButton = MutableLiveData<String>()
-
-        // gets right screen from passed position from the list
-        fun setStory(position: Int): Story {
-            currentStory = StoryList.stories[position]
-            return currentStory
+        viewModelScope.launch(Dispatchers.IO) {
+            // TODO: call the DAO method to update a forageable to the database here
+            repository.insert(storyData)
         }
 
+    }
 
-        // function to updated narration and image, returns new counter value??
-        fun updateScreen() {
-            // check that story is not at the end screen
-            narration.value = screens[counter].narration
-            image.value = screens[counter].imageID
-            leftText.value = screens[counter].leftButton
-            rightText.value = screens[counter].rightButton
-            contButton.value = screens[counter].leftButton
 
-            Log.d("COUNTER: ", counter.toString())
+    // variable to contain message whenever it gets changed/modified(mutable)
+    val narration = MutableLiveData<String>()
+    val image = MutableLiveData<Int>()
+    val leftText = MutableLiveData<String>()
+    val rightText = MutableLiveData<String>()
+    val contButton = MutableLiveData<String>()
+
+    // gets right screen from passed position from the list
+    fun setStory(position: Int): Story {
+        currentStory = StoryList.stories[position]
+        return currentStory
+    }
+
+
+    // function to updated narration and image, returns new counter value??
+    fun updateScreen() {
+        // check that story is not at the end screen
+        narration.value = screens[counter].narration
+        image.value = screens[counter].imageID
+        leftText.value = screens[counter].leftButton
+        rightText.value = screens[counter].rightButton
+        contButton.value = screens[counter].leftButton
+
+        Log.d("COUNTER: ", counter.toString())
+    }
+
+    // Increments and returns counter
+    fun incrementCounter(increment: Int): Int {
+        counter += increment
+        val storyData = StoryData(currentStory.title, counter)
+        updateCounter(storyData)
+        insert(storyData)
+        return counter
+    }
+
+    // returns true if current counter points to a choice buttons screen
+    fun isChoiceScreen(): Boolean {
+        return screens[counter].isChoice
+    }
+
+    // returns true if the current counter points to an end screen
+    fun isEndScreen(): Boolean {
+        return counter == 16 ||
+                counter == 17 ||
+                counter == 33 ||
+                counter == 47 ||
+                counter == 50 ||
+                counter == 55 ||
+                counter == 62 ||
+                counter == 66
+    }
+
+    // returns increment value based on left/right
+    fun getIncrementVal(leftClicked: Boolean): Int {
+        if (leftClicked) {
+            return screens[counter].leftValue
+        } else {
+            return screens[counter].rightValue
         }
+    }
 
-        // Increments and returns counter
-        fun incrementCounter(increment: Int): Int {
-            counter += increment
-            val storyData = StoryData(currentStory.title, counter)
-            insert(storyData)
-//        updateCounter(counter)
-            return counter
-        }
-
-        // returns true if current counter points to a choice buttons screen
-        fun isChoiceScreen(): Boolean {
-            return screens[counter].isChoice
-        }
-
-        // returns true if the current counter points to an end screen
-        fun isEndScreen(): Boolean {
-            return counter == 16 ||
-                    counter == 17 ||
-                    counter == 33 ||
-                    counter == 47 ||
-                    counter == 50 ||
-                    counter == 55 ||
-                    counter == 62 ||
-                    counter == 66
-        }
-
-        // returns increment value based on left/right
-        fun getIncrementVal(leftClicked: Boolean): Int {
-            if (leftClicked) {
-                return screens[counter].leftValue
-            } else {
-                return screens[counter].rightValue
-            }
-        }
-
-        // return counter
+    // return counter
 //    fun getCounter() : Int {
 //        return counter
 //    }
-    }
+}
 
 
 class SharedViewModelFactory( private val repository: StoryRepository) : ViewModelProvider.Factory{
